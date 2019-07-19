b9f393b3a2f8cd18dd226885b43d9a9e45698515
Nyota254 <knyota66@gmail.com>
2019-07-19 21:10:04 +0300

    add initial jQuery code to enable toggling of text with the design icon

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("#design-toggle").click(function(){
+        $("#design-icon").toggle();
+        $("#design-explanation").toggle();
+        // $(".service-icon2").toggle()
+        // $(".service-icon3").toggle()
+    });
+});
