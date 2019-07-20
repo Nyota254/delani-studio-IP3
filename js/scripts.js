787bb43a79cd20c25268de59a0ed9b48233dd075
Nyota254 <knyota66@gmail.com>
2019-07-20 14:39:55 +0300

    add proper indentation to the file and bold headings of the what we do section

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,23 @@
-$(document).ready(function(){
-    $("#design-toggle").click(function(){
+$(document).ready(function () {
+    $("#design-toggle").click(function () {
         $("#design-icon").toggle();
         $("#design-explanation").toggle();
     });
-    $("#development-toggle").click(function(){
+    $("#development-toggle").click(function () {
         $("#development-icon").toggle();
         $("#development-explanation").toggle();
     });
-    $("#product-toggle").click(function(){
+    $("#product-toggle").click(function () {
         $("#product-icon").toggle();
         $("#product-explanation").toggle();
     });
-    $(".form-style").submit(function(event){
+    $(".form-style").submit(function (event) {
         event.preventDefault();
         var name = $("#name").val();
         var email = $("#email").val();
         var text = $("#textarea").val();
-        
+
         alert(name + " we have recieved your message.Thankyou for reaching out to us.");
     });
+
 });
