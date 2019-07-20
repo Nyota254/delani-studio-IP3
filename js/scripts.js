6a5696d09e411c67a38ef9ebca534821a05759ee
Nyota254 <knyota66@gmail.com>
2019-07-20 20:11:46 +0300

    remove commented out code and correct the indentation of code

--- js/scripts.js
+++ js/scripts.js
@@ -19,44 +19,44 @@ $(document).ready(function () {
 
         alert(name + " we have recieved your message.Thankyou for reaching out to us.");
     });
-    $(".custom-sizing1").hover(function(){  
+    $(".custom-sizing1").hover(function () {
         $(".portfolio-name1").toggle();
-    }, function(){
+    }, function () {
         $(".portfolio-name1").toggle();
     });
-    $(".custom-sizing2").hover(function(){  
+    $(".custom-sizing2").hover(function () {
         $(".portfolio-name2").toggle();
-    }, function(){
+    }, function () {
         $(".portfolio-name2").toggle();
     });
-    $(".custom-sizing3").hover(function(){  
+    $(".custom-sizing3").hover(function () {
         $(".portfolio-name3").toggle();
-    }, function(){
+    }, function () {
         $(".portfolio-name3").toggle();
     });
-    $(".custom-sizing4").hover(function(){  
+    $(".custom-sizing4").hover(function () {
         $(".portfolio-name4").toggle();
-    }, function(){
+    }, function () {
         $(".portfolio-name4").toggle();
     });
-    $(".custom-sizing5").hover(function(){  
+    $(".custom-sizing5").hover(function () {
         $(".portfolio-name5").toggle();
-    }, function(){
+    }, function () {
         $(".portfolio-name5").toggle();
     });
-    $(".custom-sizing6").hover(function(){  
+    $(".custom-sizing6").hover(function () {
         $(".portfolio-name6").toggle();
-    }, function(){
+    }, function () {
         $(".portfolio-name6").toggle();
     });
-    $(".custom-sizing7").hover(function(){  
+    $(".custom-sizing7").hover(function () {
         $(".portfolio-name7").toggle();
-    }, function(){
+    }, function () {
         $(".portfolio-name7").toggle();
     });
-    $(".custom-sizing8").hover(function(){  
+    $(".custom-sizing8").hover(function () {
         $(".portfolio-name8").toggle();
-    }, function(){
+    }, function () {
         $(".portfolio-name8").toggle();
     });
 
